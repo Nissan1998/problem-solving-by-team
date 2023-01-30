1a124ef0b99fa32c9df8405c245d874c73d29b2d
shaykot43500 <hossainshaykot@gmail.com>
2023-01-30 23:32:47 +0600

    added function

--- basic-problem-solving/practice-problem.js
+++ basic-problem-solving/practice-problem.js
@@ -2,6 +2,12 @@ var teamWork = ['Shaykot','Hridoy', 'Nissan'];
 var teamWork2 = {
     shaykot:1,
     Hridoy:21,
-    Nissan:31
+    Nissan:31 
     };
-    
+
+    function sum(num1, num2){
+        const total = (num1+num2);
+        return total;
+
+    }
+    sum(12, 32);
